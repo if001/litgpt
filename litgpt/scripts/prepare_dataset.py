a1ca7bc4c69920c31ef6166cbf666a9a2e8c5a91
windows_on_wsl <otomijuf.004@gmail.com>
2024-05-30 20:35:28 +0900

    fix prepare

---
+++ litgpt/scripts/prepare_dataset.py
@@ -0,0 +1,31 @@
+from datasets import load_dataset
+import argparse
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--repo_id', required=True)
+    parser.add_argument('--output_file', required=True)
+    parser.add_argument('--key', default='text')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse()
+    print('load', args.repo_id)
+    # ds = load_dataset(args.repo_id, split="train")
+    ds = load_dataset('json', data_files=args.repo_id, split="train")
+    print(ds)
+
+    texts = []
+    for v in ds:
+        texts.append(v[args.key]+"\n")
+    
+    with open(args.output_file, 'w') as f:
+        f.writelines(texts)
+    print('end...', args.output_file)
+
+if __name__ == '__main__':
+    main()
