2640f764fe07ee9a17bcb161aa9bfce1b2b1e52f
windows_on_wsl <otomijuf.004@gmail.com>
2024-05-31 12:32:51 +0900

    fix

--- litgpt/tokenizer.py
+++ litgpt/tokenizer.py
@@ -10,8 +10,8 @@ import torch
 class Tokenizer:
     def __init__(self, checkpoint_dir: Union[Path, str]) -> None:
         checkpoint_dir = Path(checkpoint_dir)
-        if not checkpoint_dir.exists():
-            raise NotADirectoryError(f"The checkpoint directory does not exist: {str(checkpoint_dir)}")
+        # if not checkpoint_dir.exists():
+        #    raise NotADirectoryError(f"The checkpoint directory does not exist: {str(checkpoint_dir)}")
 
         self.use_bos = self.check_if_bos_token_used(checkpoint_dir)
         self.bos_id = None
