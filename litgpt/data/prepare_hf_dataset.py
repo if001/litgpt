5614f3ffff8a2d838813738a577753b9f600ad85
windows_on_wsl <otomijuf.004@gmail.com>
2024-06-04 01:35:27 +0900

    fix

--- litgpt/data/prepare_hf_dataset.py
+++ litgpt/data/prepare_hf_dataset.py
@@ -14,6 +14,7 @@ class JsonlDataRecipe(DataChunkRecipe):
     def __init__(self, tokenizer: Tokenizer, chunk_size: int):
         super().__init__(chunk_size)
         self.tokenizer = tokenizer
+        self.is_generator = False
 
     def prepare_structure(self, input_dir):
         files = Path(input_dir).rglob("*.jsonl")
