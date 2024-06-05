a037e2b15955504f1d6d31c90ec6c4251bfb7b58
windows_on_wsl <otomijuf.004@gmail.com>
2024-06-06 02:23:23 +0900

    fix

--- litgpt/data/base.py
+++ litgpt/data/base.py
@@ -79,10 +79,7 @@ class SFTDataset(Dataset):
         if self.transform is not None:
             example = self.transform(example)
         prompt = self.prompt_style.apply(prompt=example["instruction"], **example)
-        try:
-            prompt_and_response = prompt + example["output"]
-        except:
-            print('example', example)
+        prompt_and_response = prompt + example["output"]
         encoded_prompt = self.tokenizer.encode(prompt, max_length=self.max_seq_length)
         encoded_prompt_and_response = self.tokenizer.encode(
             prompt_and_response, eos=True, max_length=self.max_seq_length
