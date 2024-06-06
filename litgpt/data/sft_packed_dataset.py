99ab9a286d6b4c77c5cd9439583f8a309f384ff0
windows_on_wsl <otomijuf.004@gmail.com>
2024-06-06 21:02:44 +0900

    debug

--- litgpt/data/sft_packed_dataset.py
+++ litgpt/data/sft_packed_dataset.py
@@ -82,7 +82,7 @@ class SFTPackedDatasetHF(Alpaca):
         _dataset = Dataset.from_list(json_list)
         _dataset = _dataset.shuffle(seed=self.seed).train_test_split(test_size=self.val_split_fraction)
         self.train_dataset = _dataset['train']
-        print('before train dataset', self.test_dataset)
+        print('before train dataset', self.train_dataset)
         self.test_dataset = _dataset['test']
 
     def train_dataloader(self) -> DataLoader:
